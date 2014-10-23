7dde6b5a5a15cb7a646981f28283d5e415d730a8
Palm <palmchou@live.com>
2014-10-24 02:05:43 +0800

    Move topics#show service logic to model

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -7,9 +7,7 @@ include ApplicationHelper
     id = params[:id]
     page = current_page
     @topic = Topic.find(id)
-    not_found if page > @topic.last_page or page < 1
-    @replies = @topic.replies.paginate(page: page, per_page: Topic.replies_per_page)
-    @topic.clicks_count_up if valid_click?
+    @replies = @topic.show(page, valid_click?)
   end
 
   def new
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -25,8 +25,19 @@ class Topic < ActiveRecord::Base
     User.find_by_id(self.refresher_id)
   end
 
+  def have_page?(page)
+    page >= 1 and page <= self.last_page
+  end
+
   def last_page
     self.replies_count / Topic.replies_per_page + 1
   end
 
+  def show(page, valid_click)
+    raise ActionController::RoutingError.new('Not Found') unless have_page?(page)
+    clicks_count_up if valid_click
+    self.replies.order(created_at:
+                           :desc).paginate(page: page,
+                                           per_page: Topic.replies_per_page)
+  end
 end

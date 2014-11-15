fd59eb8db7950100ec92d19a5c31ba6c12bb40d8
WildCat <wildcat.name@gmail.com>
2014-11-15 22:41:51 +0800

    Implement hot topics worker

---
+++ app/workers/hot_topics_worker.rb
@@ -0,0 +1,14 @@
+class HotTopicsWorker
+  include Sidekiq::Worker
+
+  sidekiq_options :queue => :statistics, :retry => 5
+
+  def perform
+    logger.info('HotTopicsWorker')
+
+    @hot_topics = Topic.where('created_at > ?', 7.days.ago).order(replies_count: :desc).select('id, title').limit(10)
+
+    Rails.cache.write('hot_topics', @hot_topics)
+  end
+
+end

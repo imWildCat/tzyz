ccd805c1365c2648a4b85fe8e4e5ca2a1728f25f
WildCat <wildcat.name@gmail.com>
2014-12-01 20:20:24 +0800

    Fix bugs for unlogged users

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+  def current_user_id?(id)
+    return current_user.id == id if current_user
+    false
+  end
+  def current_user_or_not_login?(id)
+    return current_user.id == id if current_user
+    true
+  end
 end

dee2720624015e4ad21e1bdce311f0084fb9dec8
WildCat <wildcat.name@gmail.com>
2014-12-04 00:40:55 +0800

    Implement cpanel of node categories

--- app/controllers/cpanel/node_categories_controller.rb
+++ app/controllers/cpanel/node_categories_controller.rb
@@ -5,4 +5,18 @@ class Cpanel::NodeCategoriesController < Cpanel::ApplicationController
     @node_categories = NodeCategory.order(id: :asc).paginate(page: current_page, per_page: 10)
   end
 
+  def edit
+    @node_category = NodeCategory.find params[:id]
+  end
+
+  def update
+    node_category = params.require(:node_category).permit(:name, :slug)
+    if NodeCategory.update params[:id], node_category
+      flash[:success] = '节点分类更新成功。'
+    else
+      flash[:error] = '更新失败，请联系开发组。'
+    end
+    redirect_to cpanel_node_categories_path
+  end
+
 end
